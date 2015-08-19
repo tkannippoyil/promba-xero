1252170de440d5f35cacc19a343d2493e194a7d1
Tkannippoyil (Suyati) <Tkannippoyil@suyati.com>
2015-08-19 13:28:15 +0530

    cleaning tests for customers show

--- spec/apis/v1/customers_spec.rb
+++ spec/apis/v1/customers_spec.rb
@@ -4,18 +4,19 @@ require "rails_helper"
 describe Api::V1::CustomersController do
 
   describe 'index' do
+    customer =         {
+            email: "aabb@hh.de",
+            first_name: "test",
+            last_name: "1"
+        }
     before do
-      customer = Customer.create(
-          {
-              email: "aabb@hh.de",
-              first_name: "test",
-              last_name: "1"
-          }
-      )
+      Customer.create(customer)
       get :index, format: :json
     end
-    it { expect(response).to be_success }
-    it { expect(response.body).to include('aabb@hh.de') }
+    it do
+      expect(response).to be_success
+      expect(response.body).to include(customer[:email])
+    end
   end
 
   describe 'show' do
@@ -28,10 +29,10 @@ describe Api::V1::CustomersController do
       Customer.create(customer)
       get :index, format: :json
     end
-    it { expect(response).to be_success }
-    it {
+    it do
+      expect(response).to be_success
       expect(JSON.parse(response.body)[0]['email']).to eq(customer[:email])
-    }
+    end
 
   end
 

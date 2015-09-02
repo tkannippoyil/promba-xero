8e91f58ef9165a073ac06194b46be2e693f78ff3
Tkannippoyil (Suyati) <Tkannippoyil@suyati.com>
2015-09-02 18:06:24 +0530

    rename app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_promba2_session'
+Rails.application.config.session_store :cookie_store, key: '_prompa_xero_session'
